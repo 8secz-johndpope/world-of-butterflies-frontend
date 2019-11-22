12a6d7e1582a80a134a93b682a85ca67ccb00cc3
Gergely Simonics <gergelysimonics@Gergelys-MacBook-Pro.local>
2019-11-22 01:51:12 +0100

    added react-draggable...WELL, the butterflies can be finally placed in the box, but there is still LOT more to do

---
+++ src/components/product/DraggableProduct.js
@@ -0,0 +1,70 @@
+import React, {Component} from 'react';
+import Draggable from 'react-draggable'
+
+// import {DraggableCore} from "react-draggable";
+
+class DraggableProduct extends Component {
+    state = {
+        id: 0,
+        x: 0,
+        y: 0
+    };
+
+    handleDrag = (e, ui) => {
+        // console.log(ui);
+        this.setState({
+            id: this.props.id,
+            x: ui.x,
+            y: ui.y
+        });
+    };
+
+
+    handleStop = (e, ui) => {
+        console.log(e.pageX);
+        console.log(e.pageY);
+        console.log(e);
+        console.log("Stopped");
+    };
+
+    eventLogger = (e: MouseEvent, data: Object) => {
+        console.log('Event: ', e);
+        console.log('Data: ', data);
+    };
+
+    render() {
+        // const dragHandlers = {onStart: this.onStart, onStop: this.onStop};
+
+        return (
+            <React.Fragment>
+                {/*{this.props.id}*/}
+                {/*{this.props.name}*/}
+                {/*{this.props.description}*/}
+                {/*{this.props.price}*/}
+                {/*{this.props.availableQuantity}*/}
+                <Draggable
+                    bounds="parent"
+                    onDrag={this.eventLogger}
+                    onStop={this.handleStop}
+                >
+                    {/*<div>*/}
+                    {/*<div className="box">*/}
+                    <img src={serverURL + this.props.url}
+                         draggable="false"
+                         className="draggable-product-images"
+                    />
+                    {/*<div>*/}
+                    {/*    <p>{this.state.id}</p>*/}
+                    {/*    <p>{this.state.x}</p>*/}
+                    {/*    <p>{this.state.y}</p>*/}
+                    {/*</div>*/}
+                    {/*</div>*/}
+                </Draggable>
+            </React.Fragment>
+        );
+    }
+}
+
+const serverURL = "http://localhost:8080";
+
+export default DraggableProduct;
--- src/components/product/ProductMapper.js
+++ src/components/product/ProductMapper.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
-import Product from "./Product";
+import ListedProduct from "./ListedProduct";
 import {getProducts} from "../../service/fetchService/fetchService";
+import DraggableProduct from "./DraggableProduct";
 
 class ProductMapper extends Component {
+
     state = {
-        products: []
+        products: [],
+        productsToSpawnIn: [],
+        boxWidth: 840,
+        boxHeight: 700,
     };
+
 
     componentDidMount() {
         getProducts()
@@ -14,26 +20,106 @@ class ProductMapper extends Component {
             ))
     }
 
+
+    isProductArrayEmpty = () => {
+        if (!this.state.productsToSpawnIn.length) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    addProductToSpawnIn = (product) => {
+        this.setState({
+            productsToSpawnIn: [...this.state.productsToSpawnIn, product]
+        });
+        console.log(product);
+    };
+
+    widthOnChangeHandler = (e) => {
+        console.log(e.target.value);
+        this.setState({
+            boxWidth: e.target.value,
+        })
+    };
+
+    heightOnChangeHandler = (e) => {
+        console.log(e.target.value);
+        this.setState({
+            boxHeight: e.target.value,
+        })
+    };
+
 //TODO maybe delete 'type' from mapping
     render() {
+        console.log("RENDERED, H: " + this.state.boxHeight + "W:" + this.state.boxWidth);
         return (
-            <div>
-                {
-                    this.state.products.map((product, index) =>
-                        <Product
-                            id={product.id}
-                            name={product.name}
-                            description={product.description}
-                            price={product.price}
-                            availableQuantity={product.availableQuantity}
-                            type={product.type}
-                            url={product.url}
+            <React.Fragment>
+                <p>Height:</p>
+                <input type="number"
+                       onChange={this.heightOnChangeHandler}
+                       value={this.state.boxHeight}
+                />
 
-                            key={index}
-                        />
-                    )
-                }
-            </div>
+                <p>Width:</p>
+                <input type="number"
+                       onChange={this.widthOnChangeHandler}
+                       value={this.state.boxWidth}
+                />
+
+                <div className="custom-butterfly-builder">
+                    <div className="enable-scroll">
+                        {
+                            this.state.products.map((product, index) =>
+                                <ListedProduct
+                                    id={product.id}
+                                    name={product.name}
+                                    description={product.description}
+                                    price={product.price}
+                                    availableQuantity={product.availableQuantity}
+                                    type={product.type}
+                                    url={product.url}
+
+                                    addProductToSpawnIn={this.addProductToSpawnIn}
+
+                                    key={index}
+                                />
+                            )}
+                    </div>
+                    <div className="butterfly-box"
+                         style={{
+                             height: `${this.state.boxHeight}px`,
+                             width: `${this.state.boxWidth}px`,
+                             borderImageSource: 'url("http://localhost:8080/images/frames/frame2.png")'
+                         }}>
+                        {/*    border-image: url("http://localhost:8080/images/frames/frame2.png");
+*/}
+                        {/*http://localhost:8080/images/frames/dark-frame-v.jpeg*/}
+                        {/*<img src={'http://localhost:8080/images/frames/dark-frame-v.jpeg'}/>*/}
+                        {
+                            this.isProductArrayEmpty() ?
+                                null :
+
+                                this.state.productsToSpawnIn.map((product, index) =>
+                                    <DraggableProduct
+                                        id={product.id}
+                                        name={product.name}
+                                        description={product.description}
+                                        price={product.price}
+                                        availableQuantity={product.availableQuantity}
+                                        type={product.type}
+                                        url={product.url}
+
+
+                                        key={index}
+                                    />
+                                )}
+                        {/*<div style={{height: '510px', width: '510px', padding: '10px'}}>*/}
+                        {/*</div>*/}
+
+                    </div>
+                </div>
+            </React.Fragment>
         );
     }
 }
