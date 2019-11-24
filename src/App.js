eb174ead7a4d77c25e3f980e78e38a271a13d032
Gergely Simonics <gergelysimonics@Gergelys-MacBook-Pro.local>
2019-11-24 18:39:38 +0100

    created an invisible sidebar, and played with my spaghetti css to modify the header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import React, {Component} from 'react';
-import './App.css';
+import './css/App.css';
+import Header from "./components/header/Header";
 import ProductMapper from "./components/product/ProductMapper";
+
+import {Route, BrowserRouter as Router, Switch} from 'react-router-dom';
+// import Test from "./Test";
 
 class App extends Component {
 
     render() {
         return (
-            <div className="App">
-                <ProductMapper/>
-            </div>
+            <Router>
+                <div className="App">
+                    <Header/>
+                    {/*<div className="faszom" style={{paddingTop: '20px'}}>*/}
+                    {/*    <img src={'http://192.168.0.94:8080/images/butterflies/1.png'}*/}
+                    {/*         alt="LOGO"*/}
+                    {/*         className="logo-header"*/}
+                    {/*    />*/}
+                    {/*    <img src={'http://192.168.0.94:8080/images/butterflies/2.png'}*/}
+                    {/*         alt="LOGO"*/}
+                    {/*         className="logo-header"*/}
+                    {/*    />*/}
+                    {/*    <img src={'http://192.168.0.94:8080/images/butterflies/3.png'}*/}
+                    {/*         alt="LOGO"*/}
+                    {/*         className="logo-header"*/}
+                    {/*    />*/}
+                    {/*</div>*/}
+                    {/*<Switch>*/}
+                    {/*    <Route exact path="/butterfly" component={Test}/>*/}
+                    {/*</Switch>*/}
+                </div>
+            </Router>
         );
     }
 }
---
+++ src/components/header/Header.js
@@ -0,0 +1,169 @@
+import React, {Component} from 'react';
+import {Link} from "react-router-dom";
+import '../../css/Header.css';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faBars, faTimes} from "@fortawesome/free-solid-svg-icons";
+
+class Header extends Component {
+
+
+    alterNav = () => {
+        const width = window.getComputedStyle(this.refs.mySidebar).getPropertyValue("width");
+        if (width === "0px") {
+            this.refs.mySidebar.style.width = "85%";
+            this.refs.openBtnLine.style.marginLeft = "85%";
+        } else {
+            this.refs.mySidebar.style.width = "0px";
+            this.refs.openBtnLine.style.marginLeft = "0px";
+        }
+
+    };
+
+    closeNav = () => {
+        this.refs.mySidebar.style.width = "0";
+        this.refs.openBtnLine.style.marginLeft = "0";
+    };
+
+    render() {
+        return (
+            <React.Fragment>
+                <header className="header">
+                    {/*192.168.0.94*/}
+                    {/*localhost*/}
+                    <img src={'http://192.168.0.94:8080/images/logos/logoka.jpg'}
+                         alt="LOGO"
+                         className="logo-header"
+                    />
+                    <nav role="navigation" className="nav-header">
+                        <input type="checkbox" id="chk"/>
+                        <label htmlFor="chk" className="show-menu-btn">
+                            <FontAwesomeIcon className="icon-bars" icon={faBars}/>
+                        </label>
+                        <ul className="nav-options">
+
+                            <li>
+                                <Link to="/" className="main-link">HOME</Link>
+                            </li>
+                            <li>
+                                <Link to="/about" className="main-link">ABOUT</Link>
+                            </li>
+                            <li>
+                                <Link to="/butterfly" className="main-link">BUTTERFLY</Link>
+                                <ul className="dropdown">
+                                    <li><Link to="/" className="dropdown-link">MOTHS</Link></li>
+                                    <li><Link to="/" className="dropdown-link">BOTANICAL PRINTS</Link></li>
+                                    <li><Link to="/" className="dropdown-link">CREATE YOUR FRAME</Link></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <Link to="/insect" className="main-link">INSECT</Link>
+                                <ul className="dropdown">
+                                    <li><Link to="/" className="dropdown-link">BOTANICAL PRINT</Link></li>
+                                    <li><Link to="/" className="dropdown-link">CREATE YOUR FRAME</Link></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <Link to="/fossil" className="main-link">FOSSIL</Link>
+                                <ul className="dropdown">
+                                    <li><Link to="/" className="dropdown-link">FOSSIL</Link></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <Link to="/minerals" className="main-link">MINERALS</Link>
+                                <ul className="dropdown">
+                                    <li><Link to="/" className="dropdown-link">MINERAL 1</Link></li>
+                                    <li><Link to="/" className="dropdown-link">MINERAL 2</Link></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <Link to="/jewelry" className="main-link">JEWELRY</Link>
+                                <ul className="dropdown">
+                                    <li><Link to="/" className="dropdown-link">BUTTERFLY</Link></li>
+                                    <li><Link to="/" className="dropdown-link">BEATL ASSECUALL</Link></li>
+
+                                </ul>
+                            </li>
+                            <li><Link to="/contact" className="main-link">CONTACT</Link></li>
+                            <label htmlFor="chk" className="hide-menu-btn">
+                                <FontAwesomeIcon icon={faTimes}/>
+                            </label>
+                        </ul>
+                    </nav>
+                    <div id="mySidebar" ref="mySidebar" className="sidebar">
+                        <a className="close-btn" onClick={this.closeNav}>×</a>
+                        <div id="sidebar-content">
+                            <ul className="main-menu">
+                                <li><Link to="/">HOME</Link></li>
+                                <li><Link to="/about">ABOUT</Link></li>
+
+
+                                <li><Link id="butterfly-main" className="sidebar-main-link"
+                                          to="/butterfly">BUTTERFLY</Link>
+                                    <label title="Toggle Drop-down" className="drop-icon" htmlFor="sm1">▾</label>
+                                    <input type="checkbox" id="sm1"/>
+                                    <ul className="sub-menu">
+                                        <li><Link to="/">MOTHS</Link></li>
+                                        <li><Link to="/">BOTANICAL PRINTS</Link></li>
+                                        <li><Link to="/">CREATE YOUR FRAME</Link></li>
+                                    </ul>
+                                </li>
+
+
+                                <li><Link id="insect-main" className="sidebar-main-link" to="/insect">INSECT</Link>
+                                    <label title="Toggle Drop-down" className="drop-icon" htmlFor="sm2">▾</label>
+                                    <input type="checkbox" id="sm2"/>
+                                    <ul className="sub-menu">
+                                        <li><Link to="/">BOTANICAL PRINT</Link></li>
+                                        <li><Link to="/">CREATE YOUR FRAME</Link></li>
+                                    </ul>
+                                </li>
+
+
+                                <li><Link id="fossil-main" to="/fossil" >FOSSIL</Link>
+                                    <label title="Toggle Drop-down" className="drop-icon" htmlFor="sm3">▾</label>
+                                    <input type="checkbox" id="sm3"/>
+                                    <ul className="sub-menu">
+                                        <li><Link to="/">FOSSIL SOMETHING</Link></li>
+                                    </ul>
+                                </li>
+
+
+                                <li><Link id="mineral-main" to="/mineral">MINERAL</Link>
+                                    <label title="Toggle Drop-down" className="drop-icon" htmlFor="sm4">▾</label>
+                                    <input type="checkbox" id="sm4"/>
+                                    <ul className="sub-menu">
+                                        <li><Link to="/">MINERAL 1</Link></li>
+                                        <li><Link to="/">MINERAL 2</Link></li>
+                                    </ul>
+                                </li>
+
+
+                                <li><Link id="jewelry-main" to="/jewelry">JEWELRY</Link>
+                                    <label title="Toggle Drop-down" className="drop-icon" htmlFor="sm5">▾</label>
+                                    <input type="checkbox" id="sm5"/>
+                                    <ul className="sub-menu">
+                                        <li><Link to="/">BUTTERFLY</Link></li>
+                                        <li><Link to="/">BEATL ASSECUALL</Link></li>
+                                    </ul>
+                                </li>
+
+
+                                <li><Link to="/contact">CONTACT</Link></li>
+                            </ul>
+                        </div>
+                        {/*<a href="#">About</a>*/}
+                        {/*<a href="#">Services</a>*/}
+                        {/*<a href="#">Clients</a>*/}
+                        {/*<a href="#">Contact</a>*/}
+                    </div>
+
+                    <div id="open-btn-line" ref="openBtnLine">
+                        <button className="open-btn" onClick={this.alterNav}>☰</button>
+                    </div>
+                </header>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Header;
