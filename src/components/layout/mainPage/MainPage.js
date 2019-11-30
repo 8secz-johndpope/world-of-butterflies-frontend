06434c4c3acffdf4755d45150ad561233518b174
Gergely Simonics <gergelysimonics@Gergelys-MBP.lan>
2019-11-30 17:50:52 +0100

    added two headlines to the main page

--- src/components/layout/mainPage/MainPage.js
+++ src/components/layout/mainPage/MainPage.js
@@ -3,6 +3,7 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import SlideShow from "./SlideShow";
 import CategoryMapper from "./CategoryMapper";
 import FeaturedProductMapper from "./FeaturedProductMapper";
+import OneLineText from "./OneLineText";
 
 class MainPage extends Component {
 
@@ -11,7 +12,17 @@ class MainPage extends Component {
             <React.Fragment>
                 <SlideShow/>
                 <CategoryMapper/>
+                <OneLineText
+                    text='BESTSELLERS'
+                    paddingTop='0'
+                    paddingBottom='40px'
+                />
                 <FeaturedProductMapper/>
+                <OneLineText
+                    text='NOVINKY'
+                    paddingTop='40px'
+                    paddingBottom='60px'
+                />
             </React.Fragment>
         );
     }
---
+++ src/components/layout/mainPage/OneLineText.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class OneLineText extends Component {
+    render() {
+        return (
+            <div className="one-line-text-container"
+                 style={{
+                     paddingTop:`${this.props.paddingTop}`,
+                     paddingBottom:`${this.props.paddingBottom}`
+                 }}>
+                <p className="one-line-text">{this.props.text}</p>
+            </div>
+        );
+    }
+}
+
+export default OneLineText;
